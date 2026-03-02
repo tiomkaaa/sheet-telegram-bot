69892288e6c24d83f663fb15c0e419db6a1ed148
tiomkaaa <gerasym90@gmail.com>
2026-03-02 18:52:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import time
 import hashlib
 import os
 
-BOT_TOKEN = os.environ.get("8694170751:AAEANq-5I_dDQRORLMPO72Kb6UtAU_Uyqz0")
-CHAT_ID = os.environ.get("547293821")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1h-i-_RS2GxWkIH4iZwNLNV3NSUEB4fz9xyIWrJIhwxg/export?format=csv&gid=168784301"
 
@@ -18,9 +18,9 @@ while True:
 
         if last_hash and current_hash != last_hash:
             requests.get(
-                f"https://api.telegram.org/bot{8694170751:AAEANq-5I_dDQRORLMPO72Kb6UtAU_Uyqz0}/sendMessage",
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
                 params={
-                    "chat_id": 547293821,
+                    "chat_id": CHAT_ID,
                     "text": "⚡ Є зміни в аркуші НОВИНКИ!"
                 }
             )
